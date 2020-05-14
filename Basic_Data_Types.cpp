9dafd8f7d4a3e1cacb98c8dfc39cf789ec38f40d
Divyansh <57042143+Divyanshwick@users.noreply.github.com>
2020-05-14 20:20:33 +0530

    Create Basic_Data_Types.cpp

---
+++ Basic_Data_Types.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int a;
+    long int b;
+    char c;
+    float d;
+    double e;
+    cin >> a >> b >> c >> d >> e;
+    printf("%d\n%ld\n%c\n%.3f\n%.9lf",a,b,c,d,e);
+    // Complete the code.
+    return 0;
+}
