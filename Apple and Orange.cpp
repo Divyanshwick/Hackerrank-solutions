548906ea53c9d38cb7ef0d1f40f6cc21e3cdea9e
Divyansh <57042143+Divyanshwick@users.noreply.github.com>
2020-05-23 15:19:51 +0530

    Create Apple and Orange.cpp

---
+++ Apple and Orange.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int s,t,a,b,i,cnt1=0,cnt2=0,m,n;
+    cin >> s >> t >> a >> b >> m >> n;
+    int apples[m],oranges[n];
+    
+   
+
+    
+
+    
+}
