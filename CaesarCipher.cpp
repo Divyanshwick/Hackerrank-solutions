a22eb58dba64449f97e8c43b5eced6058705bc1e
Divyansh <ddivyansh9999@gmail.com>
2020-06-22 14:37:13 +0530

    Update CaesarCipher.cpp

--- CaesarCipher.cpp
+++ CaesarCipher.cpp
@@ -5,18 +5,26 @@ int main(){
     int n,k;
     cin >> n;
     string str;
-    char temp;
     cin >> str;
     cin >> k;
+    k%=26;
     for(int i=0;i<n;i++){
+        int temp=str[i];
         
-        if((str[i]>=65 && str[i]<=90)||(str[i]>=97 && str[i]<=122)){
-            temp=str[i]+k;               
-            if((temp>=90 && temp<97) || (temp>122)){
-                str[i]+=(k-26);
+        if(str[i]>=97 && str[i]<=122){
+            temp+=k;
+            if(temp > 122){
+                str[i] = 96+(temp%122);
             }
             else str[i]+=k;
-            
+        }
+        else if(str[i] >=65 && str[i] <= 90){
+            temp+=k;
+            if(temp > 90){
+                str[i] = 64+(temp%90);
+            }
+            else str[i]+=k;
+
         }
     }
     cout << str;
