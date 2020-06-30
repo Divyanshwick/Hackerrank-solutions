c8b5d307092ce11ed901256ad847d3add9073aea
Divyansh <ddivyansh9999@gmail.com>
2020-06-30 18:15:12 +0530

    Create The-Love-Letter-Mystery.cpp

---
+++ The-Love-Letter-Mystery.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the theLoveLetterMystery function below.
+int theLoveLetterMystery(string s) {
+    int i,j,cnt=0;
+    for(i=0;i<s.length();i++){
+        if(s[i] < s[s.length()-(i+1)]){
+            cnt += s[s.length()-(i+1)] - s[i];
+        }
+    }
+    return cnt;
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int q;
+    cin >> q;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int q_itr = 0; q_itr < q; q_itr++) {
+        string s;
+        getline(cin, s);
+
+        int result = theLoveLetterMystery(s);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
